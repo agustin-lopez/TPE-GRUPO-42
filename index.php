04bce60c838b57a570832e2a47ab3545f9695d33
Ignacio <agueraignacio19@gmail.com>
2022-11-03 20:47:30 -0300

    agrego lista de peliculas/series al index y agrego sus respectivos estilos

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="estilos.css">
     <title>Series</title>
 </head>
@@ -24,10 +25,10 @@
             <a class="nav-link" href="#">Series</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
+            <a class="nav-link" href="#">Peliculas</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
+            <a class="nav-link" href="#">Categorias</a>
           </li>
         </ul>
         <form class="d-flex" role="search">
@@ -46,8 +47,29 @@
                 <button class="boton">Informacion</button>
             </div>
         </div>
+    <div class="peliculas-recom">
+      <div class="contenedor-titulo-controles">
+        <h3>Peliculas/series recomendadas</h3>
+        <div class="indicadores"></div>
+      </div>
+      <div class="contenedor-principal">
+        <button role="button" id="flecha-izq" class="flecha-izq"><i class="bi bi-arrow-left-circle-fill"></i></button>
+        <div class="contenedor-carrousel">
+          <div class="carrousel">
+            <div class="pelicula">
+              <a href="#"><img src="./Imagenes-pagina/pelicula_1.jpg" alt=""></a>
+              <a href="#"><img src="./Imagenes-pagina/pelicula_2.jpg" alt=""></a>
+              <a href="#"><img src="./Imagenes-pagina/pelicula_3.jpg" alt=""></a>
+              <a href="#"><img src="./Imagenes-pagina/pelicula_4.jpg" alt=""></a>
+            </div>
+          </div>
+        </div>
+        <button role="button" id="flecha-der" class="flecha-der"><i class="bi bi-arrow-right-circle-fill"></i></button>
+      </div>
+    </div>
     </main>
     <footer></footer>
+    <script src="./script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>
